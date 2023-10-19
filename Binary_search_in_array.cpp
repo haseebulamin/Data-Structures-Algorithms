1c54fd7442841367eec6311c143643f91624786f
haseebulamin <haseebulamin07@gmail.com>
2023-10-19 23:53:27 +0500

    Linear search in array

---
+++ Binary_search_in_array.cpp
@@ -0,0 +1,25 @@
+// Binary search in array 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[5] = {7,6,3,9,5};
+    int num, id=-1;
+    cout<<"Enter number you wanna search: ";
+    cin>>num;
+    for (int i = 0; i<5; i++)
+    {
+        if(a[i]==num)
+        {
+            id=i;
+        }
+    }
+    if (id==-1)
+    {
+        cout<<"Number not found.";
+    }
+    else{
+        cout<<"Number found at index of: "<<id;
+    }
+    return 0;
+}
