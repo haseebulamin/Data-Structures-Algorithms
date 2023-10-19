acb0fab7af2f4fcea27dab04c1853c553560bec3
haseebulamin <haseebulamin07@gmail.com>
2023-10-20 01:11:46 +0500

    traversing od array

---
+++ Traversing_of_array.cpp
@@ -0,0 +1,15 @@
+// traversing od array
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {5,9,3,6,2};
+    // for traversing
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+    return 0;
+}
