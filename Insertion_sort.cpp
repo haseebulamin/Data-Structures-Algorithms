5012967a55ab52d21f58f7d39de9425b2397ad19
haseebulamin <haseebulamin07@gmail.com>
2023-12-15 22:27:28 +0500

    Insertion sort in array

---
+++ Insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout<<"Enter a size of Array: ";
+    cin>>size;
+    int arr[size];
+    cout<<"Enter a elements in array.\n";
+    for (int i = 0; i < size; i++)
+    {
+        cin>>arr[i];
+    }
+    // insertion sort 
+    for (int i = 1; i < size; i++)
+    {
+        int current = arr[i];
+        int j = i-1;
+        while (arr[j] > current && j>=0)
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = current;
+    }
+    cout<<"Insertion sort array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
